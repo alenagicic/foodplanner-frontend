141f4838f868b0b84684639ff9f7896f15c6375f
alenagicic <alenagicic@gmail.com>
2025-10-05 15:32:37 +0200

    Redeploy with changes to index

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 💡 FIX: Set the base path to your GitHub repository name 
-  // This ensures assets (JS/CSS) are loaded from the correct subdirectory.
   base: '/matabas-frontend/', 
 });
