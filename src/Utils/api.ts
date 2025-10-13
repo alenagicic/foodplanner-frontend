d099c4cd11d86a04803c8ad97afc19fc995f203a
alenagicic <alenagicic@gmail.com>
2025-10-13 04:13:49 +0200

    Further debugging for signin error.

--- src/Utils/api.ts
+++ src/Utils/api.ts
@@ -1,5 +1,12 @@
-const apiUrl =  import.meta.env.VITE_API_URL;
-const API_KEY = import.meta.env.VITE_API_KEY; 
+const apiUrl: string = (import.meta.env.VITE_API_URL as string);
+const API_KEY: string = (import.meta.env.VITE_API_KEY as string);
+
+if (!apiUrl) {
+    console.error("VITE_API_URL is missing! Check build step and .env configuration.");
+    throw new Error("API URL is not configured.");
+}
+
+console.log(apiUrl)
 
 export interface Recipe {
     Id: string;
